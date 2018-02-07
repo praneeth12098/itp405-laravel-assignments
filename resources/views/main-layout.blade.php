bb291ba969cb848c5bcfbec4c34198cb33462716
praneeth12098 <pbatte@usc.edu>
2018-02-06 18:44:48 -0800

    Abstracted Main Layout

---
+++ resources/views/main-layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<title>@yield('title')</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	</head>
+	<body>
+		@yield('content')
+	</body>
+</html>
--- resources/views/playlist-list.blade.php
+++ resources/views/playlist-list.blade.php
@@ -1,19 +1,14 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset='utf-8'>
-		<title>Playlists</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-	</head>
-	<body>
-		<ul>
-			@foreach($playlists as $playlist)
-				<li>
-					<a href="/playlists/{{$playlist->PlaylistId}}">
-						{{$playlist->Name}}
-					</a>
-				</li>
-			@endforeach
-		</ul>
-	</body>
-</html>
+@extends('main-layout')
+@section('title', 'Playlists')
+
+@section('content')
+	<ul>
+		@foreach($playlists as $playlist)
+			<li>
+				<a href="/playlists/{{$playlist->PlaylistId}}">
+					{{$playlist->Name}}
+				</a>
+			</li>
+		@endforeach
+	</ul>
+@endsection
